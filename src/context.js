edc584ae4458b60f4b84f21f3f6725bc1a7e8dcf
akashshirfule <akash.shirfule@yash.com>
2022-09-29 22:49:40 +0530

    29-sept

--- src/context.js
+++ src/context.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 
-const API_URl = `http://www.omdbapi.com/?apikey=b89c61e4&s=titanic`;
+export const API_URl = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
 
 const AppContext = React.createContext();
 
@@ -8,20 +8,23 @@ const AppProvider = ({ children }) => {
 
     const [isLoading,setIsLoading]=useState(true);
     const [movie,setMovie]=useState([]);
-    const [iserror,setIsError]=useState({show:true,msg:""})
+    const [iserror,setIsError]=useState({show:true,msg:""});
+    const [query,setQuery]=useState("titanic")
 
 
   const getMovies = async (url) => {
+    setIsLoading(true);
     try {
       const res =await fetch(url);
       const data =await res.json();
       console.log(data);
       if(data.Response==="True"){
         setIsLoading(false);
+        setIsError({show:false,msg:""})
         setMovie(data.Search)
       }
       else{
-        setIsError({show:true,msg:data.error})
+        setIsError({show:true,msg:data.Error})
       }
     } catch (error) {
       console.log(error);
@@ -29,11 +32,15 @@ const AppProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    getMovies(API_URl);
-  }, []);
+   let Timerout= setTimeout(()=>{
+        getMovies(`${API_URl}&s=${query}`);
+    },800)
+    
+    return ()=>clearTimeout(Timerout)
+  }, [query]);
 
   return (
-    <AppContext.Provider value={{isLoading,movie,iserror}}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{isLoading,movie,iserror,query,setQuery}}>{children}</AppContext.Provider>
   );
 };
 
